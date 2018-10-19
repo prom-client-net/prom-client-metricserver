939883642a7cb0dc0f7c866c85db010f7c9055bc
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2018-10-19 15:58:32 +0300

    Fixed issue when HttpListener required '/' at the end of host url

--- src/Prometheus.Client.MetricServer/Defaults.cs
+++ src/Prometheus.Client.MetricServer/Defaults.cs
@@ -3,7 +3,7 @@
     internal static class Defaults
     {
         public const string Host = "*";
-        public const string MapPath = "/metrics";
+        public const string MapPath = "/metrics/";
         public const bool UseDefaultCollectors = true;
         public const string ContentType = "text/plain; version=0.0.4";
     }
