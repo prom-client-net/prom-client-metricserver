2607eb0e6e03bd63b6acf6107dcbb8a277e9d8d5
Sergey Kuznetsov <cmt@phnx47.net>
2019-12-21 01:55:12 +0700

    Fix pack.sh, add missed icon.png

--- pack.sh
+++ pack.sh
@@ -6,6 +6,6 @@ if ${APPVEYOR_REPO_TAG} == "true"  ; then
   dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.MetricServer.Kestrel -c Release --include-symbols --include-source --no-build -o artifacts/nuget
 else
   echo "Pack for MyGet"
-  dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.MetricServer -c Release ---include-symbols --include-source --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts/myget
+  dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.MetricServer -c Release --include-symbols --include-source --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts/myget
   dotnet pack ${APPVEYOR_BUILD_FOLDER}/src/Prometheus.Client.MetricServer.Kestrel -c Release --include-symbols --include-source --no-build --version-suffix build${APPVEYOR_BUILD_NUMBER} -o artifacts/myget
 fi
